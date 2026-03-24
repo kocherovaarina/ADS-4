9f966443852346abff874c48e94179c379a1450c
kocherovaarina <arinakocerova@gmail.com>
2026-03-24 13:10:13 +0300

    Task done by Kocherova Arina from 25 IST 4-2

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,26 @@
 // Copyright 2021 NNTU-CS
+#include <algorithm>
+#include <iostream>
+#include <unordered_set>
+#include <unordered_map>
+
 int countPairs1(int *arr, int len, int value) {
-  return 0;
-}
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
+    std::sort(arr, arr + len);
+    int count = 0;
+    int left = 0;
+    int right = len - 1;
+
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == value) {
+            if (arr[left] == arr[right]) {
+                int n = right - left + 1;
+                count += n * (n - 1) / 2;
+                break;
+            } else {
+                int left_count = 1, right_count = 1;
+                while (left + 1 < right && arr[left] == arr[left + 1]) {
+                    left++;
+                    left_count++;
+                }
+                while (right - 1 > left
